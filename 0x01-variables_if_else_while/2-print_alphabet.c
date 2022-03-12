df35be0bb409d843c6b08ee4dfc6cb93305e7c8a
TheSalient <abdulazeezlawal236@gmail.com>
2022-03-12 13:20:09 +0000

    the next updated version

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char a = 'a';
-int b = 0;
-/*your code goes here*/
-while (b < 26)
-{
-	putchar(a);
-	a++;
-	b++;
-}
-putchar('\n');
-return (0);
+	char a = 'a';
+	int b = 0;
+	/*your code goes here*/
+	while (b < 26)
+	{
+		putchar(a);
+		a++;
+		b++;
+	}
+	putchar('\n');
+	return (0);
 }
