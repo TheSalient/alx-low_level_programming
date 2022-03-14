ff520e601def2806607526ccbd472b7a65c73d6c
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-14 09:04:21 +0100

    updated version

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
-int main(void) /* this is the main function */
+int main(void)
+/* this is the main function */
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle \n");
+    return (0);
 }
