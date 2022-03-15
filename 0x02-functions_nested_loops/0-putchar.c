783ba4c52c223b743c1b2febefc5e1caa84ddb0e
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-15 15:19:47 +0100

    updated version

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,17 @@
 #include "main.h"
-/* 
-* This is where the libraries are.
-*/
-int _putchar(char c);
-
-int main(void)
+int _putchar(char c); /* This is where the libraries are. */
+int main(void) /*This is where the main function begins*/
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return(0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+    return 0;
 }
 
 
