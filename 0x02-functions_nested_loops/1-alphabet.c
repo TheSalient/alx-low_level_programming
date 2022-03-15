b29af5ea6679177d4676b6fc0ec0f23edf7a476a
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-15 17:33:42 +0100

    I added the progarm that prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 int _putchar(char c);
 
-int main(void)
+void print_alphabet(void)
 {
-void print_alphabet(void);
-{
-    int c = 97;
-    while(c < 123)
-        _putchar('c');
-}
+char c = 97;
+while(c < 123)
+    _putchar('c');
 _putchar('\n');
 return (0);
 }
+
