6fe6d7df2d8c6fb6fe3182df0de3d189bca27fe7
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-15 17:27:25 +0100

    updated main file by putting the header there

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
+#include "1-alphabet.c"
 /**
 * _putchar - This is used to output the character.
 * @c: This is a character
 * @return: int
 */
 int _putchar(char c);
- void print_alphabet(void);
+void print_alphabet(void);
