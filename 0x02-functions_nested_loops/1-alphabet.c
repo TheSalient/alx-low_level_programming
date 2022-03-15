0d3f3023dec23e74e9a9f9743e631b3d5f3edb48
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-15 16:59:33 +0100

    I added the progarm that prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _putchar(char c);
+
+int main(void)
+{
+void print_alphabet(void);
+{
+    int c = 97;
+    while(c < 123)
+        _putchar('c');
+}
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
-
-/*_putchar function prototype*/
+/**
+* _putchar - This is used to output the character.
+* @c: This is a character
+* @return: int
+*/
 int _putchar(char c);
+ void print_alphabet(void);
