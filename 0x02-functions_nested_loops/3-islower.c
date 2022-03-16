51969e8c47bb17bb1f3401ebb8ae982ded2458a8
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 12:59:12 +0100

    updated main.h file and added 3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - This is a function
+ * Return : Always 0 and 1
+ */
+int _islower(int c)
+{
+if (c >= 97 && c < 123)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int _putchar(char c);
 void print_alphabet(void);
 int print_last_digit(int);
 void print_alphabet_x10(void);
+int _islower(int c);
