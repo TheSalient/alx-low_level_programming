5e0e3b2aa7978d5eb9cb2e2a6ffc6685cecd0d3c
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 10:58:08 +0100

    This program print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,12 @@
 * @n: This is the parameter
 * return: int
 */
-int print_last_digit(int n) 
+int print_last_digit(int n)
 {
 int l;
 l = n % 10;
 if (l < 0)
-{
-l = l * -1;
-}
+    l = l * -1;
 _putchar(l + '0');
 return (l);
 }
