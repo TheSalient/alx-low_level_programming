b367492c2a01421b72073f86b07abe11252493cf
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 18:56:42 +0100

    i added the add program

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _abs- This is a function islower.
- * @c, @b: This is the parameter.
+ * add- This is a function islower.
+ * @c : This is the parameter.
+ * @b : This is the parameter.
  * Return: Always 0 and 1 for true and false.
  */
 int add(int c, int b)
