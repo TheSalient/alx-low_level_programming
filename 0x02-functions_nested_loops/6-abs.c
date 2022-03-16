49467d0dbff3ba69553213c977c79366ee1662e6
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 17:50:55 +0100

    i added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs- This is a function islower.
+ * @c: This is the parameter.
+ * Return: Always 0 and 1 for true and false.
+ */
+int _abs(int c)
+{
+if (c >= 0)
+{
+return (c);
+}
+else
+{
+c = c * -1;
+return (c);
+}
+}
