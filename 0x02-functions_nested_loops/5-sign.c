4855dba783046d74f1d0f4cd9d2c9f7095ba0de4
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 17:39:07 +0100

    program 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha- This is a function islower.
+ * @n: This is the parameter.
+ * Return: Always 0 and 1 for true and false.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
