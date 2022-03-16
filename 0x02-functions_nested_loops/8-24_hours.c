d9723bba605a907810b2354a3e09767c6e620a41
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 18:15:32 +0100

    i added 24 program to the file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_last_digit - This is a function
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+int h2, h1, m2, m1;
+for (h2 = 0; h2 < 3; h2++)
+{
+for (h1 = 0; h1 <= 9; h1++)
+{
+for (m2 = 0; m2 < 6; m2++)
+{
+for (m1 = 0; m1 <= 9; m1++)
+{
+_putchar(h2 + '0');
+_putchar(h1 + '0');
+_putchar(':');
+_putchar(m2 + '0');
+_putchar(m1 + '0');
+if (m1 == 10)
+{
+m1 = 0;
+}
+}
+if (m2 == 6)
+{
+m2 = 0;
+}
+}
+if (h1 == 4 && h2 == 2)
+{
+h1 = 10;
+}
+}
+}
+}
