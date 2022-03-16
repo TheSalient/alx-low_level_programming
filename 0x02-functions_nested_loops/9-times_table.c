f5e4e9a313cd7088c5e0f3aa217eb7367fe7f6f7
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-16 18:31:36 +0100

    times table program

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * times_table - This is a function
+ * Return: Always 0
+ */
+void times_table(void)
+{
+int c, d, z;
+for (c = 0; c < 10; c++)
+{
+for (d = 0; d < 10; d++)
+{
+z = c * d;
+_putchar(z);
+}
+_putchar('\n');
+}
+}
