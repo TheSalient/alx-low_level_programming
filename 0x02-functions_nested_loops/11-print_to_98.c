6eccd50f65944b23a02b075545c9ddeca822d0f8
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 14:23:28 +0100

    printing to 98 program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,24 +6,37 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
+int i;
+for (i = n; i <= 98; i++)
 {
-while (n <= 98)
+if (i < -9 )
 {
-return (n);
+_putchar('-');
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
+}
+else if (i<0 && i >= -9)
+{
+_putchar('-');
+_putchar('0' + i);
+}
+else if (i >= 0 && i < 10)
+{
+_putchar(i);
+}
+else if (i >= 10 && i < 100)
+{
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
 _putchar(',');
 _putchar(' ');
-n++;
 }
-}
-else
+else if (i >= 100)
 {
-while (n >= 98)
-{
-return (n);
+_putchar('0' + (i / 100));
+_putchar('0' + (i % 10));
 _putchar(',');
 _putchar(' ');
-n--;
 }
 }
 }
