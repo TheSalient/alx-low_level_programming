81d70f5a20b97e2b9a425e01580e28603b3fbca4
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 12:10:49 +0100

    printing to 98 program

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - This is a function
+ * @n: This is the parameter
+ * Return: Always l
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+_putchar(n + '0');
+_puthchar(',');
+_putchar(' ');
+n++;
+} 
+else if (n > 98)
+{
+_putchar(n + '0');
+_puthchar(',');
+_putchar(' ');
+n--;
+}
+else
+{
+_putchar(n + '0');
+}
+}
