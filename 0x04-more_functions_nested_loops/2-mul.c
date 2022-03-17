a9bb9b217e66df5469f0b06ab206ecabf0d4fa3d
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 18:37:29 +0100

    added 2-mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - This is a function
+ * @a: This is the parameter
+ * @b: This is the parameter
+ * Return: Always l and n
+ */
+int mul(int a, int b)
+{
+int c = a * b;
+printf("%d\n", c);
+}
