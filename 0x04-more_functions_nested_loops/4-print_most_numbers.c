3daf602fc345857a602cf8981db36f7826d26471
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 18:57:11 +0100

    added 4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- * print_numbers - This is a function
+ * print_most_numbers - This is a function
  * Return: Always l and n
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int n;
 for (n = 0; n <= 9; n++)
 {
+if (n != 2 && n !=4)
+{
 _putchar(n);
+}
 }
 _putchar('\n');
 }
