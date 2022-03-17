d4fa64908bf7517e84e59b5df4d945ddbbe08121
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 18:30:10 +0100

    i added the 1-isdigit file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isupper - This is a function
+ * _isdigit - This is a function
  * @c: This is the parameter
  * Return: Always l and n
  */
 int _isdigit(int c)
 {
-if (c <= 0 && c <= 9)
+if (c >= 0 && c <= 9)
 return (1);
 else
 {
