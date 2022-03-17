5dfc12551b57054398ab3635d77c16d461f7b745
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-17 12:18:24 +0100

    printing to 98 program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,20 @@ void print_to_98(int n)
 {
 if (n < 98)
 {
-_putchar(n + '0');
+_putchar('n');
 _putchar(',');
 _putchar(' ');
 n++;
-} 
+}
 else if (n > 98)
 {
-_putchar(n + '0');
+_putchar('n');
 _putchar(',');
 _putchar(' ');
 n--;
 }
 else
 {
-_putchar(n + '0');
+_putchar('n');
 }
 }
