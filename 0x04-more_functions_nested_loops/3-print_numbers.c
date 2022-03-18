b669b33553f91652a98e55b7fe59926770683281
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 15:36:16 +0100

    hi

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 /**
  * print_numbers - This is a function
  * 
- * Return: Always 0
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
 int n;
 for (n = 0; n <= 9; n++)
 {
-_putchar(n);
+_putchar(n + '0');
 }
 _putchar('\n');
 }
