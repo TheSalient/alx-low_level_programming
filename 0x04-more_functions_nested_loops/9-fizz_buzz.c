5ba612fb9697b791b58533a006d61bf284033a56
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 09:58:49 +0100

    fizzbuzz in c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main - This is a function
+ * 
+ * Return: Always 0
+ */
+int main()
+{
+void fizzbuzz_c(void);
+fizzbuzz_c();
+return 0;
+}
+
+/**
  * fizzbuzz_c - This is a function
  * 
  * Return: Always 0
  */
 void fizzbuzz_c(void)
 {
-int i; 
+int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
