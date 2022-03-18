f5fdf4077744556e07f10550bd0cac3f2137a95d
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 09:02:34 +0100

    The diagonal program.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - This is a function
+ * @n: This is the parameter
+ * Return: Always l and n
+ */
+void print_diagonal(int n)
+{
+int i = 0;
+int k = 0;
+if (n > 0)
+{
+while (i < n)
+{
+while(k < i)
+{
+_putchar(' ');
+k++;
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+k = 0;
+}
+}
+else
+{
+_putchar('\n');
+}
+}
