3bcf177dd48139c0f77f311ede0ae43234c51d8c
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 08:31:43 +0100

    i added a straight line program

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - This is a function
+ * @n: This is the parameter
+ * Return: Always l and n
+ */
+void print_line(int n)
+{
+int i =0;
+if (n > 0)
+{
+while (i < n)
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
