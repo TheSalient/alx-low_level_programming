8a39e80cac48520ddebced9df5325263f4b12e94
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 08:23:18 +0100

    Hi, thi is me

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,17 @@
  */
 void more_numbers(void)
 {
-int n;
+int n, c;
+for (c = 0; c < 10; c++)
+{
 for (n = 0; n <= 14; n++)
 {
-if (n < 10)
-{
-_putchar(n + '0');
-}
-else
+if (n >= 10)
 {
 _putchar((n / 10) + '0');
+}
 _putchar((n % 10) + '0');
 }
+_putchar('\n');
 }
 }
