9ab860ed11bd0b63017a670ca392fee53d668aa8
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 11:40:14 +0100

    fizzbuzz in c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main - This is a function
  * 
@@ -25,22 +25,22 @@ for (i = 1; i <= 100; i++)
 if (i % 15 == 0)
 {
 printf("FizzBuzz");
-_putchar(' ');
+putchar(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-_putchar(' ');
+putchar(' ');
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-_putchar(' ');
+putchar(' ');
 }
 else
 {
 printf("%d", i);
-_putchar(' ')
+putchar(' ');
 }
 }
 }
