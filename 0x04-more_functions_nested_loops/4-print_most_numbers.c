b79dd34b3dba5c73fd8fcf93b21f15cc902b3b9b
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-18 15:29:08 +0100

    i updated print number ten times

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,10 @@ void print_most_numbers(void)
 int n;
 for (n = 0; n <= 9; n++)
 {
-if (n != 2 && n != 4)
+int c = n;
+if (c != 2 && c != 4)
 {
-_putchar(n + '0');
+_putchar(c + '0');
 }
 }
 _putchar('\n');
