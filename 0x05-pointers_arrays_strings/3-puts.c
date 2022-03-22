805b04036ad3ec9d40b379b185537330fbda1497
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-22 12:35:26 +0100

    program that print str to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *@a: This is the parameter.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
