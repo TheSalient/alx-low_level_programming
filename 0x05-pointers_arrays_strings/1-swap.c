324237b68746de8b25ed3503f9eb486f0ef334e3
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-22 12:05:05 +0100

    a program that parameters

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *@a: This is the parameter.
+ @b: This is the parameter.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int p;
+p = *a;
+*a = *b;
+*b = *a;
+}
