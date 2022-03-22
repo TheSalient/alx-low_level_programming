0eb618e4357441e96599541e7acfc4b58a7a5bbd
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-22 18:44:49 +0100

    added some programs

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - a function that takes a pointer to an int as parameter and
+ * @a: chaine of caractere
+ * @n: The second parameter
+ * Return: 1 or 0
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+if (n > 0)
+{
+while (i < n - 1)
+{
+printf("%d, ", a[i]);
+i++;
+}
+printf("%d", a[i]);
+}
+printf("\n");
+}
