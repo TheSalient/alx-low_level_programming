4a59bca0a3995361d8108cb48e8dff3811b286b0
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-22 13:02:17 +0100

    a program that reverses string to stdout.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - check the code
+ *@s: This is the parameter.
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i = 1;
+while (s[i])
+{
+i++;
+}
+for (i; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
