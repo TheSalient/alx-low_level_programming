835aa97da3089990267f0e906fb14c55e030aff6
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-28 10:44:36 +0100

    a memset-like function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - a function ...
+ * @s: char
+ *@b: char
+ *@n: unsigned int
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
