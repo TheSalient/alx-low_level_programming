181c4b2821a013393ff14ee2f41f5f7d1a38a5c7
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-30 10:06:11 +0100

    recursion folder

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function ...
+ * @s: the string parameter
+ *
+ * Return: 1 or 0
+ */
+void _puts_recursion(char *s)
+{
+prinf("%s\n", s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+/**
+* _putchar - This is used to output the character.
+* @c: This is a character
+* Return: int
+*/
+int _putchar(char c);
+void _puts_recursion(char *s);
