c8e3b1dd103f90ef9e4736f26c01b8632fe744df
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-30 19:26:43 +0100

    recursion folder

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* isPalRec - function
+*
+* @s: the chaine
+* @b: another parameter
+* @e: another parameter
+* Return: Always 0.
+*/
+int isPalRec(char *s, int b, int e)
+{
+if (b == e)
+{
+return (1);
+}
+if (s[b] != s[e])
+{
+return (0);
+}
+if (b < (e + 1))
+{
+return isPalRec(s, b + 1, e - 1);
+}
+return (1);
+}
+/**
+* is_palindrome - function
+*
+* @s: the chaine
+* Return: Always 0.
+*/
+int is_palindrome(char *s)
+{
+ int n = strlen(s);
+if (n == 0)
+{
+return (1);
+}    
+return isPalRec(*s, 0, (n - 1));
+}
