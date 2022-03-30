5bf61e483f2b96bc06eae33d3170f7f0a0a8b94f
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-30 17:22:28 +0100

    recursion folder

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* factorial - function
+*
+* @n: the chaine
+* Return: Always 0.
+*/
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n-1));
+}
+}
