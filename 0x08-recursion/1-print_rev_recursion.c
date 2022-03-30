33c7d50ae10b13dfafd88b08aece14182f19f17d
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-03-30 18:40:39 +0100

    recursion folder

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * _puts_recursion - a function ...
+ * _print_rev_recursion - a function ...
  * @s: the string parameter
  *
  * Return: 1 or 0
  */
 void _print_rev_recursion(char *s)
 {
-char i, var, ch;
-var = strlen(s);
-if (i < var / 2)
+if (*s != '\0')
 {
-ch =  s[i];
-s[i] = s[var - i - 1];
-s[var - i - 1] = ch;
-i++;
-_print_rev_recursion(s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
