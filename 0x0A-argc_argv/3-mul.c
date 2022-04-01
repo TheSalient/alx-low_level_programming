4db9c5619ca3417912b8dabb51239d31463df0f1
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-04-01 18:34:00 +0100

    working with argc, and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,11 @@
 */
 int main(int argc, char **argv)
 {
+int p;
 if (argc == 3)
 {
-printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+p = (atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", p);
 }
 else
 {
