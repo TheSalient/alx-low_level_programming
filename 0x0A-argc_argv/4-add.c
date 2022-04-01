c461712da3a30ae5ab6073c73dd2a8d37c5e4787
Lawal Azeez Tosin <abdulazeezlawal236@gmail.com>
2022-04-01 16:36:11 +0100

    working with argc, and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,9 +15,17 @@ int i;
 int p = 0;
 for (i = 1; i < argc; i++)
 {
-p += argv[1];
+if (argv[i] == 0 && argv[i] <= 9)
+{
+p += argv[i];
+}
+else
+{
+printf("Error\n");
+return (1);
 }
 printf("%d\n", p);
+}
 }
 else if (argc == 1)
 {
