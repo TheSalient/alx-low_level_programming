11b14440db8a3f8140996d918fc3d462b60a46a1
TheSalient <48091348+TheSalient@users.noreply.github.com>
2022-04-02 16:57:02 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* ft_atoi - function
+*
+* @str: the chaine
+* Return: Always 0.
+*/
+
+int		ft_atoi(const char *str)
+{
+	int		n;
+	int		sign;
+
+	n = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' ||
+			*str == '\n' || *str == '\f')
+		str++;
+	sign = *str == '-' ? -1 : 1;
+	if (sign == -1 || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9')
+			n = n * 10 + (*str - '0');
+		else
+		{
+			printf("Error\n");
+			exit(EXIT_SUCCESS);
+			/*break*/
+		}
+		str++;
+	}
+	return (n * sign);
+
+}
+
 /**
 * main - function
 *
-* @argc: the chaine
-*@argv: another parameter
+* @argv: the chaine of chaine
+* @argc: the int
 * Return: Always 0.
 */
+
 int main(int argc, char **argv)
 {
-if (argc >= 2)
-{
-int i;
-int p = 0;
-for (i = 1; i < argc; i++)
-{
-if (atoi(argv[i]) == 0 && atoi(argv[i]) <= 9)
-{
-p += atoi(argv[i]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-printf("%d\n", p);
-}
-}
-else if (argc == 1)
-{
-printf("0\n");
-}
-{
-printf("Error");
-}
-return (0);
+	int i, som = 0, a;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a = ft_atoi(argv[i]);
+		if (a >= 0)
+			som = som + a;
+	}
+	printf("%d\n", som);
+	return (0);
 }
