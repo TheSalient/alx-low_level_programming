9a51b2fffafdd2d259b9404007df9956213f14f7
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-04 14:53:20 +0100

    copying memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
+#if defined _LIBC || defined STDC_HEADERS
 #include <string.h>
+#include <stdlib.h>
+#else
+char *malloc();
+char *memcpy;
+#endif
 /**
  * _strdup - the function that print char in specific number.
  * @s: the size to print.
@@ -12,9 +15,8 @@ char * _strdup(const char *s)
 {
 size_t len = strlen(s) + 1;
 void *new = malloc(len);
-if (s == NULL)
+if (new == NULL)
 {
-printf("failed to allocate memory\n");
 return (NULL);
 }
 return ((char *) memcpy(new, s, len));
