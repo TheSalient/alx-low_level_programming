2a51411a31fea4d71982d60e65f94d69550ea300
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-04 12:28:03 +0100

    The next item, malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,13 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		printf("failed to allocate memory");
+	}
 	s = malloc((size + 1) * sizeof(char));
 	if (s == 0)
 	{
-		printf("failed to allocate memory");
 		return (NULL);
 	}
 	i = 0;
