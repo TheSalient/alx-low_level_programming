d0a9daf3f7c47f7934f3870425acf630bfe461c5
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-05 13:14:05 +0100

    copying memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	char *src;
 	int len = 0, i = 0, len2 = 0, i2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = ""
 	while (s1[len])
 		len++;
 	while (s2[len2])
