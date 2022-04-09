d8745cde61a7c3633d509d78a77bf92365a3794e
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-09 14:22:09 +0100

    The multiply task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+unsigned int *multiply(int num1, int num2)
+{
+    if (isdigit(num1) && isdigit(num2))
+    {
+        unsigned int *c;
+        c = malloc(sizeof(int) * num1);
+        c = num1 * num2;
+        return (c);
+    }
+    else
+    {
+        printf("Error");
+        exit(98); 
+    }
+}
+
+unsigned int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("Error");
+        exit(98);
+    }
+    unsigned int d;
+    d = multiply(argv[0], argv[1]);
+    printf("%i\n", d);
+} 
