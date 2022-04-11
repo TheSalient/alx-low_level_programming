2a18e2ea86b108bb1b990af3a75e6aac5811421e
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-11 16:22:33 +0100

    README and dog header.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * 
+ * Description: This is struct has three parameter of float and char * types.
+ */
+struct dog
+{
+    char * name;
+    float age;
+    char * owner;
+    /* data */
+};
+#endif
+
