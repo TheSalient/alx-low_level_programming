ef106373d37529db98549c26b19830e148e10b52
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-11 16:41:21 +0100

    initialing a structure.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function ...
+ * @d: the pointeur of structure
+ * @name: the char
+ * @age: the int
+ * @owner: the char
+ *
+ * Return: 1 or 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d -> name = name;
+    d->age = age;
+    d->owner = owner;
+}
