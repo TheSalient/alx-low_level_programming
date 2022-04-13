9ee5cdf383389de6cae4522d748713562ea42e6c
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-13 12:15:04 +0100

    i commited a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - The function name
+ * @name: first parameter.
+ * @f: function pointer to void.
+ * 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
