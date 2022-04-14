4ec67f6c9b709f2690581323371146b77ad0d7b9
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-14 13:36:04 +0100

    corrected betty mistakes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,24 +2,28 @@
 #include "function_pointers.h"
 
 /**
- * int_index - The function. 
- * 
- * @param array: the first parameter
- * @param size: the second parameter
- * @param cmp: the callback function 
- * @return int 
+ * int_index - The function.
+ * @array: the first parameter
+ * @size: the second parameter
+ * @cmp: the callback function
+ *
+ * Return: -1 if false, something else otherwise
  */
 
 int int_index(int *array, size_t size, int (*cmp)(int))
 {
-    size_t i;
+size_t i;
 
-    if (size <= 0 || array == NULL || cmp == NULL)
-        return (-1); 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]))
-            return (i);
-    }
-    return (-1);
+if (size <= 0 || array == NULL || cmp == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
 }
