3b7930d112722d74f1813b4f5b504f9a7a7411f7
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-22 00:26:32 +0100

    i first singly linked file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - the function that prints
+ * @h: The parameter 
+ * 
+ * Return: size_t 
+ */
+size_t print_list(const list_t *h)
+{
+size_t the = 0;
+    
+while (h)
+{
+if (!h->str)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+the++;
+}
+
+return (the);
+}
