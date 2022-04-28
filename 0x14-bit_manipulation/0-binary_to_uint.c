97e20a495dfc99c0ecd458369466251555f0719b
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-28 21:57:00 +0100

    the next stage.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int a = 0;
-        int i;
+unsigned int a = 0;
+int i;
 
-        if (!b)
-        {
-                return (0);
-        }
+if (!b)
+{
+return (0);
+}
 
-        for (i = 0; b[i]; i++)
-        {
-                if (b[i] < "0" && b[i] > 0)
-                {
-                        return (0);
-                }
-                a = 2 * a + (b[i] - '0');
-        }
-        return a;
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' && b[i] > '1')
+{
+return (0);
+}
+a = 2 * a + (b[i] - '0');
+}
+return (a);
 }
