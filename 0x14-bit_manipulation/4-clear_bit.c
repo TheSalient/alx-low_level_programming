bdcdca903a029025c20561d144c161a5aa4dfee6
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-29 17:12:27 +0100

    corrected function 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * clear_bit - the function that sets the value pof a bit to 0 at a given index. 
+ * clear_bit - the function that sets the value of a bit to 0 at a given index.
  * @n: the first parameter.
  * @index: the second parameter.
  *
@@ -8,11 +8,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index > 63)
-{
-return (-1);
-}
-*n = (~(1UL << index) & *n);
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = (~(1UL << index) & *n);
 
-return (-1);
+	return (1);
 }
