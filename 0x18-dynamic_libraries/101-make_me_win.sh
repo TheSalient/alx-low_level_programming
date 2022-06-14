aa64815cf3f7fe0929d7a2d9c29bb7ee19b60374
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-14 15:03:23 +0100

    hi

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/TheSalient/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -p .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
